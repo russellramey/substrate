4891932a83a30648784974fc548da537eec36674
Russell Ramey <doublercreative@gmail.com>
2022-03-16 10:45:27 -0500

    added admin/editor script function

--- _includes/theme-scripts.php
+++ _includes/theme-scripts.php
@@ -3,7 +3,7 @@
 *** Scripts
 	Add scripts/styles to theme
 ************************************************************************************/
-// Setup function
+// Theme front-end scripts
 add_action( 'wp_enqueue_scripts', 'substrate_theme_load_scripts' );
 function substrate_theme_load_scripts() {
 	// Jquery
@@ -11,5 +11,11 @@ function substrate_theme_load_scripts() {
 
 	// Site functions
 	wp_enqueue_script( 'theme-functions', get_stylesheet_directory_uri() . '/_assets/themejs.js', '', true );
+}
+// Theme dashboard/editor scripts
+add_action( 'admin_enqueue_scripts', 'substrate_theme_load_admin_scripts', 1 );
+function substrate_theme_load_admin_scripts() {
+	// Editor functions
+	wp_enqueue_script( 'editor-functions', get_stylesheet_directory_uri() . '/_assets/editorjs.js', '', filemtime( plugin_dir_path( dirname(__FILE__) ) . '_assets/editorjs.js' ));
 }
 ?>
