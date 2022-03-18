1bb3bf51b135986730bf989d632e3d05ae175c74
Russell Ramey <doublercreative@gmail.com>
2022-03-18 09:55:28 -0500

    added filter to typography to allow child theme additional sizes

--- _includes/theme-typography.php
+++ _includes/theme-typography.php
@@ -1,20 +1,21 @@
 <?php
 /************************************************************************************
-*** Theme color pallete
-	Available colors and color classes for block editor...
+*** Theme font sizes
+	Available font sizes for block editor...
 ************************************************************************************/
 // Setup function
 function substrate_theme_typography() {
 	// Disable custom font sizes
     add_theme_support( 'disable-custom-font-sizes' );
 	// Editor Font Sizes
-	add_theme_support('editor-font-sizes', array(
+	add_theme_support('editor-font-sizes', apply_filters('editor-font-sizes', array(
         array('name' => 'Smaller', 'size' => 10, 'slug' => 'smaller'),
 		array('name' => 'Small', 'size' => 12, 'slug' => 'small'),
 		array('name' => 'Medium', 'size' => 14, 'slug' => 'medium'),
         array('name' => 'Large', 'size' => 21, 'slug' => 'large'),
 		array('name' => 'Larger', 'size' => 36, 'slug' => 'larger'),
-    ));
+        array('name' => 'X-Large', 'size' => 48, 'slug' => 'x-large'),
+    )));
 }
 // Add setup to theme
 add_action( 'after_setup_theme', 'substrate_theme_typography' );
